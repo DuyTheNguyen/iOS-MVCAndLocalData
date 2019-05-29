9466123cf0ba85e343748a9ee97bcf0a1ff25dc5
Dave <duy27295@gmail.com>
2019-05-29 17:53:38 +1000

    add person model

---
+++ iOS_MVCAndLocalData/iOS_MVCAndLocalData/Model/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  iOS_MVCAndLocalData
+//
+//  Created by THE DUY NGUYEN on 29/5/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+struct Person: Decodable{
+    let name: String?
+    let occupation: String?
+    let address: String?
+}
